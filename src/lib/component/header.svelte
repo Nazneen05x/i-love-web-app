1995d6146ab1adc0aa5f87f32ad9bc8966dcd551
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2024-01-22 23:41:05 +0100

    Eerste opzet
    Ik heb een begin gebouwd van mijn We love app blog

---
+++ src/lib/component/header.svelte
@@ -0,0 +1,35 @@
+<header>
+  <ul>
+    <li><p>i</p></li>
+    <li><img src="/love.png" alt="love" width="50em" height="auto" /></li>
+    <li><p>web-app</p></li>
+    <li><a href="#"></a>About me</li>
+    <li><a href="#"></a>Github</li>
+  </ul>
+</header>
+
+<style>
+  header {
+    margin: 0;
+    padding: 0;
+    box-shadow: 0px 7px 6px -5px rgba(0, 0, 0, 0.1);
+  }
+  
+  ul {
+    list-style: none;
+    display: flex;
+    align-content: center;
+    align-items: center;
+    gap: 1em;
+    font-family: "Courier New", Courier, monospace;
+    text-transform: uppercase;
+    font-size: 20px;
+  }
+
+  p {
+    font-family: "Courier New", Courier, monospace;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 20px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,60 @@
 <script>
-    import Header from "../lib/component/header.svelte";
+  import Header from "../lib/component/header.svelte";
 </script>
 
-<Header/>
+<Header />
+<main>
+  <section class="creamecolor">
+    <img src="/keyboard.jpg" alt="spider-web" />
+    <img class="mouse" src="/mouse.jpg" alt="mouse" />
+  </section>
+
+  <section class="bluecolor">
+    <img class="speaker"  src="/speaker.jpg" alt="speaker">
+  </section>
+</main>
+
+<style>
+  .creamecolor {
+    background-color: #f7dfdf;
+    width: 15em;
+    height: 25em;
+    padding: 1em;
+    margin: 1em;
+    margin-left: 14em;
+    margin-top: -0.5em;
+    border-top-left-radius: 1em;
+    border-bottom-left-radius: 1em;
+  }
+
+  img {
+    width: 20em;
+    height: auto;
+    margin-left: -9em;
+    border-radius: 2em;
+    padding: 1em;
+  }
+
+  .mouse {
+    margin-left: -15em;
+  }
+
+  .bluecolor{
+    background-color: #9FD6E3;
+    width: 15em;
+    height: 25em;
+    padding: 1em;
+    margin: 1em;
+    margin-left: -1em;
+    margin-top: 8.5em;
+    border-top-right-radius: 1em;
+    border-bottom-right-radius: 1em;
+    position: relative;
+  }
+
+  .speaker{
+    margin-left: 2em;
+    margin-top: 4em;
+    width: 24em;
+  }
+</style>
